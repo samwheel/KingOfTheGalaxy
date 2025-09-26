cdb8f8d5bf56f21ae5d3aedcc30bbe7463c14719
samwise <samuel.wheelwright@gmail.com>
2025-09-26 12:08:15 -0600

    Added production_icon.

--- pygame_helper/button.py
+++ pygame_helper/button.py
@@ -1,4 +1,4 @@
-from pygame import font, Color, Surface, draw, sprite
+from pygame import font, Color, Surface, sprite
 import pygame
 
 class Button(sprite.Sprite):
@@ -29,4 +29,29 @@ class Button(sprite.Sprite):
         self.surface = Surface(self.rect.size)
         self.surface.fill("gray")
         self.surface.blit(self.font.render(self.text, True, self.color), (5, 5))
+        surface.blit(self.surface, self.rect)
+
+class ImageButton(sprite.Sprite):
+    def __init__(self, color: Color, image: Surface, x: int, y: int, action = lambda: None) -> None:
+        super().__init__()
+        self.color: Color = color
+        self.surface: Surface = image
+        self.rect: pygame.Rect = self.surface.get_rect()
+        self.rect.topleft = (x, y)
+        self.x: int = x
+        self.y: int = y
+        self.action = action
+    
+    def move(self, x:int, y:int) -> None:
+        self.rect.topleft = (x + self.x, y + self.y)
+    
+    def update(self, event) -> None:
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                self.action()
+    
+    def draw(self, surface: Surface) -> None:
+        background: Surface = Surface(self.rect.inflate(10, 10).size)
+        background.fill(self.color)
+        surface.blit(background, self.rect)
         surface.blit(self.surface, self.rect)
--- pygame_main.py
+++ pygame_main.py
@@ -1,9 +1,16 @@
+from os import environ
+environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 import pygame
 from pygame.time import Clock
+
+from pygame_helper import button
+
 from view.star import Star
 from view.planet import PlanetView
+
 from src.planet import Planet, Focus
 from src.race import *
+from src.empire import Empire
 
 pygame.init()
 
@@ -11,18 +18,27 @@ screen: pygame.Surface = pygame.display.set_mode((1280,720))
 pygame.display.set_caption("King of the Galaxy")
 planet_group = pygame.sprite.Group()
 with open("src/races.dat", "r") as f:
-    races = [eval(line) for line in f.readlines()]
-    planet_group.add(PlanetView(30, pygame.Color(20, 255, 20), Planet("Earth", races[0], 1, Focus.INDUSTRY)))
-    planet_group.add(PlanetView(30, pygame.Color(72, 25, 5), Planet("Mars", races[0], 1, Focus.INDUSTRY)))
+    races: list[Race] = [eval(line) for line in f.readlines()]
+player_empire = Empire("Blorg", "Glorp")
+player_empire.planets.append(Planet("Earth", races[0], 1, Focus.INDUSTRY))
+player_empire.planets.append(Planet("Mars", races[0], 1, Focus.INDUSTRY))
+
+
+planet_group.add(PlanetView(30, pygame.Color(20, 255, 20), player_empire.planets[0]))
+planet_group.add(PlanetView(30, pygame.Color(72, 25, 5), player_empire.planets[1]))
 star: Star = Star(640, 360, 10, pygame.Color(255, 0, 0), planet_group)
 stars: pygame.sprite.Group = pygame.sprite.Group()
 stars.add(star)
 planet_view_group = pygame.sprite.Group()
+widgets = pygame.sprite.Group()
+production_button = button.ImageButton(pygame.Color("black"), pygame.image.load("images/production_icon.png"), 50, 50, action=lambda: print(player_empire.industry))
+widgets.add(production_button)
 
 clock = Clock()
 
 while True:
     for event in pygame.event.get():
+        widgets.update(event)
         planet_group.update(event)
 
         if event.type == pygame.QUIT:
@@ -33,7 +49,7 @@ while True:
             clicked_stars = [s for s in stars if s.rect.collidepoint(event.pos)]
             if clicked_stars:
                 planet_view_group.empty()
-                planet_view_group.add([star.planets for star in clicked_stars])
+                planet_view_group.add([planet for star in clicked_stars for planet in star.planets])
         
 
     screen.fill("black")
@@ -47,6 +63,10 @@ while True:
         pygame.draw.rect(screen, (50, 50, 50), pygame.rect.Rect(900, 50, 350, 600))
         for planet_view in planet_view_group:
             planet_view.draw(screen)
+    
+    for widget in widgets:
+        widget.move(0, 0)
+        widget.draw(screen)
 
     pygame.display.flip()
     clock.tick(60)
