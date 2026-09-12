72f7ed7f56b5285d5f240f7aec5774330e3d6443
samwise <samwheel@users.noreply.github.com>
2026-09-11 22:03:47 -0600

    Modified main.py

--- backend/main.py
+++ backend/main.py
@@ -153,5 +153,7 @@ def create_app() -> Flask:
 
 print(f"[bold green]Game generated with {len(current_game.starmap)} stars, {len(current_game.tech_tree)} technologies, and {len(current_game.empires)} empire{len(current_game.empires) != 1 and 's' or ''}.[/bold green]")
 
+app = create_app()
+
 if __name__ == "__main__":
-    create_app().run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
